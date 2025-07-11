8b5838ae6fa4bd4e2c8008fc85b256620609b92e
Zp1021 <90364004+Zp1021@users.noreply.github.com>
2025-07-11 01:01:22 -0400

    Implementing button with example of state

--- src/components/Display.jsx
+++ src/components/Display.jsx
@@ -1,9 +1,20 @@
+import { useState } from "react";
+
 
 export default function Display() {
 
+    const [name, setName] = useState('Mario')
+
+    const handleClick = () => {
+        setName('Luigi');
+    }
+
     return (
         <div>
-            <button id="nextBtn">
+            <p id="charName">
+                {name}
+            </p>
+            <button onClick={handleClick} id="nextBtn">
                 It's time to get Schwifty!!!
             </button>
         </div>
